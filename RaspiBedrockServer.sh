4c7f33e0923fed0b86a2b847a4096ad57e470c74
EditDungeon <94791797+EditDungeon@users.noreply.github.com>
2025-02-17 19:07:34 +0000

    Update RaspiBedrockServer.sh

--- RaspiBedrockServer.sh
+++ RaspiBedrockServer.sh
@@ -2,7 +2,7 @@
 
 ## Bedrock Server Installation Script for ARM64
 
-sudo apt install curl jq unzip -y # Install necessary dependencies for this script
+sudo apt install curl jq unzip screen systemd -y # Install necessary dependencies
 
 # box64 installation function
 function Installbox64() {
@@ -146,10 +146,39 @@ cd /minecraft/scripts # Open scripts folder
 # Download server scripts
 echo "Downloading server scripts..."
 curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/start.sh
-curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/stop.sh
 curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/restart.sh
 curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/update-server.sh
 curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/update-scripts.sh
 echo "Finished downloading scripts."
+
+# Install systemd service
+cd /etc/systemd/system
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/MinecraftBedrock.service
+sudo systemctl daemon-reload
+
+# Prompt auto-starting the server
+echo "Would you like to start the server on boot? (y/n)" 
+read auto_start
+
+if [ $auto_start = "y" || $auto_start = "Y" ]
+then
+    sudo systemctl enable MinecraftBedrock.service
+fi
+
+# Register alias commands
+
+alias "BedrockServer start"=sudo systemctl start MinecraftBedrock.service
+alias "BedrockServer stop"=sudo systemctl stop MinecraftBedrock.service
+alias "BedrockServer restart"=sudo systemctl restart MinecraftBedrock.service
+alias "BedrockServer console"=screen -r MinecraftBedrock
+
+# Installation finished
+ 
+echo "Finished installation"
+echo "Use 'BedrockServer start' to start the server"
+echo "Use 'BedrockServer stop' to stop the server"
+echo "Use 'BedrockServer restart' to restart the server"
+echo "Use 'BedrockServer console' to view the server console"
+
 
 
