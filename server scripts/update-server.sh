c859708439f06e75ae73abbc8feb82fba2bef592
EditDungeon <94791797+EditDungeon@users.noreply.github.com>
2025-02-17 18:43:05 +0000

    Update update-server.sh

--- server scripts/update-server.sh
+++ server scripts/update-server.sh
@@ -1,1 +1,25 @@
 #!/bin/bash
+
+InstalledServerType=cat server_type
+InstalledServerVersion=cat version
+
+if [ $InstalledServerType = "release" ]
+then
+    LatestVersion=`curl -s https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/versions.json | jq -r '.linux.stable'`
+
+    if [ $InstalledServerVersion != $LatestVersion ]
+    then
+        curl -A "Mozilla/5.0" -o "BedrockServer.zip" "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$LatestVersion.zip"
+        unzip -d /minecraft BedrockServer.zip
+    fi
+
+elif [ $InstalledServerType = "preview" ]
+then
+    LatestVersion=`curl -s https://raw.githubusercontent.com/Bedrock-OSS/BDS-Versions/main/versions.json | jq -r '.linux.preview'`
+
+    if [ $InstalledServerVersion != $LatestVersion ]
+    then
+        curl -A "Mozilla/5.0" -o "BedrockServer.zip" "https://www.minecraft.net/bedrockdedicatedserver/bin-linux/bedrock-server-$LatestVersion.zip"
+        unzip -d /minecraft BedrockServer.zip
+    fi
+fi
