53536946b864cc34c87649d8c3808ce3a2e1687b
EditDungeon <94791797+EditDungeon@users.noreply.github.com>
2025-02-17 18:11:26 +0000

    Update update-scripts.sh

---
+++ server scripts/update-scripts.sh
@@ -0,0 +1,7 @@
+echo "Downloading server scripts..."
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/start.sh
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/stop.sh
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/restart.sh
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/update-server.sh
+curl https://github.com/EditDungeon/RaspiBedrockServer/raw/refs/heads/main/server%20scripts/update-scripts.sh
+echo "Finished downloading scripts."
